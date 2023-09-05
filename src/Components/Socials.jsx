ea74197c5ca9a0ab8f06b0cdd63e13af9bff48f4
Brian Chebon <brynnlamura@gmail.com>
2023-09-05 18:17:57 +0300

    social links git and linkedin

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { AiOutlinePhone, AiOutlineMail, AiFillInstagram, AiFillGithub, AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai'
+import { AiOutlinePhone, AiOutlineMail, AiFillGithub, AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai'
 
 import '../styles/Socials.css';
 
@@ -9,35 +9,31 @@ const Socials = () => {
     <div className='socials'>
       <h2>Find Me @ The Web</h2>
       <div className="social">
-      <div className='git'>
-        <AiOutlinePhone />
-        <p>Contact</p>
-        <a href="tel:+254721318162">+254 721 318 162</a>
+       <div className='link'>
+          <AiFillLinkedin />
+          <p>Linkedin</p>
+          <a href="https://www.linkedin.com/in/brian-chebon-8b081219a" target='_blank' rel='noreferrer'>Let's Connect</a>
+        </div>
+       <div className='git'>
+       <AiFillGithub />
+          <p>Github</p>
+          <a href="https://github.com/Chebon-breezy" target='_blank' rel='noreferrer'>Let's Connect</a>
+        
         </div>
         <div className='insta'>
           <AiOutlineMail />
             <p>Gmail</p>
             <a href="mailto:brynnlamura@gmail.com" target='_blank' rel='noreferrer'>Send Email</a>
         </div>
-        <div className='insta'>
-          <AiFillInstagram />
-          <p>Instagram</p>
-          <a href="https://www.instagram.com/briannchebon" target='_blank' rel='noreferrer'>Let's Connect</a>
-        </div>
         <div className='git'>
-          <AiFillGithub />
-          <p>Github</p>
-          <a href="https://github.com/Chebon-breezy" target='_blank' rel='noreferrer'>Let's Connect</a>
+        <AiOutlinePhone />
+        <p>Contact</p>
+        <a href="tel:+254721318162">+254 721 318 162</a>
         </div>
         <div className='twit'>
           <AiFillTwitterCircle />
           <p>Twitter</p>
           <a href="https://twitter.com/@Chebon254" target='_blank' rel='noreferrer'>Let's Connect</a>
-        </div>
-        <div className='link'>
-          <AiFillLinkedin />
-          <p>Linkedin</p>
-          <a href="https://www.linkedin.com/in/brian-chebon-8b081219a" target='_blank' rel='noreferrer'>Let's Connect</a>
         </div>
       </div>
     </div>
