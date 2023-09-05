5ad4d5ceacec9b15ef5169dd00b326c9480353ef
Brian Chebon <brynnlamura@gmail.com>
2023-09-05 19:54:29 +0300

    added the tittle name

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,7 +5,7 @@ import '../styles/About.css'
 const About = () => {
   const calculate_age = () => {
     var today = new Date();
-    var birthDate = new Date("1994-10-16");
+    var birthDate = new Date("1998-07-18");
     var age_now = today.getFullYear() - birthDate.getFullYear();
     var m = today.getMonth() - birthDate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
