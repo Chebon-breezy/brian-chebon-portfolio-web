768645e15e1eed100db9528d79c00c9701e8af2f
Brian Chebon <brynnlamura@gmail.com>
2023-09-05 18:46:46 +0300

    added project links

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,19 +5,19 @@ import '../styles/Projects.css';
 const Projects = () => {
   const projectsInfo = {
     one: {
-      name: 'Project 1',
-      desc: 'congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed',
-      liveLink: 'website url',
-      githubLink: 'https://github.com/',
-      techs: ['HTML', 'CSS', 'TypeScript'],
+      name: 'Food Recipe App',
+      desc: 'This web application is designed to simplify your culinary adventures by providing a vast array of recipes at your fingertips. Leveraging the power of React for the frontend and connecting to an API for real-time ingredient information, this app enhances your cooking journey.',
+      liveLink: 'https://recipe-app-chebon.vercel.app',
+      githubLink: 'https://github.com/Chebon-breezy/RecipeApp',
+      techs: ['HTML', 'CSS', 'React', 'Recipe API', 'JavaScript'],
       isOpenSource: true
     },
     two: {
-      name: 'Project 2',
-      desc: 'congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed',
-      liveLink: 'website url',
-      githubLink: 'https://github.com/',
-      techs: ['React', 'Node', 'MongoDB', 'Socket.io', 'Redux'],
+      name: 'Event App',
+      desc: 'This web application serves as a hub for events from various cities, allowing users to explore, discover, and book their favorite events. Built using JavaScript, Next.js, HTML, and CSS, this app provides a seamless event browsing experience.',
+      liveLink: 'https://event-app-chebon.vercel.app',
+      githubLink: 'https://github.com/Chebon-breezy/Event-App',
+      techs: ['JavaScript', 'Next.js', 'HTML' , 'CSS'],
       isOpenSource: true
     }
   }
