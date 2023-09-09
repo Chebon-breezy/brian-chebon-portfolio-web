dab633c3341f93e2082b496b5556109b25a1341c
Brian Chebon <brynnlamura@gmail.com>
2023-09-09 18:11:53 +0300

    added the resume link

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
   const textContent = useRef();
   const handleDownload = () => {
     saveAs(
-      "google drive url",
+      "https://drive.google.com/file/d/1TuW_I8XBHitCxCtnzeR4fvLB47X-G6Ur/view?usp=sharing",
       "Resume.pdf"
     )
   }
