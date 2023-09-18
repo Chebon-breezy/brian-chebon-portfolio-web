a7235175c4dc11b3fe69e6c408bbbcae5e25c545
Brian Chebon <brynnlamura@gmail.com>
2023-09-18 14:27:31 +0300

    added resthot project

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -12,7 +12,17 @@ const Projects = () => {
       techs: ['HTML', 'CSS', 'React', 'Recipe API', 'JavaScript'],
       isOpenSource: true
     },
+
     two: {
+      name: 'RestHotel',
+      desc: 'RestHotel is a Next.js and TypeScript web application designed for hotels, created with HTML and Tailwind CSS. It offers a seamless guest experience with user-friendly menus, Chefs Specials, a contact form, and easy booking options. Guests can access hotel information, make inquiries, and explore the menu with ease.',
+      liveLink: 'https://resthotel-next-js-ts-main.vercel.app/',
+      githubLink: 'https://github.com/Chebon-breezy/resthotel-nextjs-ts-main',
+      techs: ['TypeScript', 'Tailwind CSS', 'HTML' , 'CSS', 'Next JS '],
+      isOpenSource: true
+    },
+
+    three: {
       name: 'Event App',
       desc: 'This web application serves as a hub for events from various cities, allowing users to explore, discover, and book their favorite events. Built using JavaScript, Next.js, HTML, and CSS, this app provides a seamless event browsing experience.',
       liveLink: 'https://event-app-chebon.vercel.app',
@@ -20,7 +30,7 @@ const Projects = () => {
       techs: ['JavaScript', 'Next.js', 'HTML' , 'CSS'],
       isOpenSource: true
     },
-    three: {
+    four: {
       name: 'Landing Page',
       desc: 'Landing Page – a visually captivating, one-page masterpiece that combines JavaScript, HTML, and CSS to create an engaging user experience. Tailwind CSS adds a touch of style and modernity to the design, while Masonry Grid and Fancybox CSS enhance image presentation and interactivity.',
       liveLink: 'https://adrespect-page-bon.netlify.app',
